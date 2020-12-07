713031d4c30cf1755451c1fd4b0e18e1d50323ad
Antonio Morettini <antoniomorettini@MacBook-Air-de-Antonio.local>
2020-12-07 09:45:51 +0000

    use jitter function

--- analysis_data.R
+++ analysis_data.R
@@ -4,5 +4,7 @@ library(ggplot2)
 lockdown_data <- read_xlsx("life-under-lockdown-w2-extract.xlsx", sheet = 1)
 
 ggplot (data = lockdown_data) +
-  geom_point(mapping = aes(x = age, y = beliefs))
+  geom_point(mapping = aes(x = age, y = beliefs),
+  position = "jitter"
+)
   
