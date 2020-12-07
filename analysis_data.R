05bea2e423c6e58af2da38bb834bab6335ea8251
Antonio Morettini <antoniomorettini@MacBook-Air-de-Antonio.local>
2020-12-07 09:42:04 +0000

    run a first analysis, ggplot

---
+++ analysis_data.R
@@ -0,0 +1,8 @@
+library(readxl)
+library(ggplot2)
+
+lockdown_data <- read_xlsx("life-under-lockdown-w2-extract.xlsx", sheet = 1)
+
+ggplot (data = lockdown_data) +
+  geom_point(mapping = aes(x = age, y = beliefs))
+  
